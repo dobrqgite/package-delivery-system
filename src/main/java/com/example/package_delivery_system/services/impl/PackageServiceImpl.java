6dd6863ff681f5e9047b2d3ee8b7b560f1d93dca
George <georgik1_@abv.bg>
2022-04-12 12:15:51 +0300

    completed the addPackage method.Needs to be tested.

--- src/main/java/com/example/package_delivery_system/services/impl/PackageServiceImpl.java
+++ src/main/java/com/example/package_delivery_system/services/impl/PackageServiceImpl.java
@@ -26,79 +26,13 @@ public class PackageServiceImpl implements PackageService {
     @Override
     public PackageResponseDto addPackage(CreatePackageDto createPackageDto) {
 
-
-        //TODO:Not sure how to set user to package!!!!!MUST FIX!!!
-
         String content = createPackageDto.getContent();
         BigDecimal weight = createPackageDto.getWeight();
         BigDecimal height = createPackageDto.getHeight();
         BigDecimal width = createPackageDto.getWidth();
         BigDecimal length = createPackageDto.getLength();
 
-
-        //Calculate tax
-        //TODO: extract method for tax calculation
-        //Never in my time with programming have I written such utterly disgusting code...
-
-
-        //RETHINK logic -NEW METHOD FOR CALCULATION!!!!
-
-//       if (height.doubleValue() <= 61
-//               && width.doubleValue() <= 44
-//               && length.doubleValue() <= 37) {
-//           if (weight.doubleValue() >= 1 && weight.doubleValue() <= 10){
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 5.5)));
-//           }else if(weight.doubleValue() >= 10 && weight.doubleValue() <= 20){
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 4.5)));
-//           }else if(weight.doubleValue() >= 20 && weight.doubleValue() <= 50) {
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 3)));
-//           }else{
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 1.5)));
-//           }
-//       } else if (height.doubleValue() >= 61
-//               && width.doubleValue() >= 44
-//               && length.doubleValue() >= 37
-//               && height.doubleValue() <= 90
-//               && width.doubleValue() <= 90
-//               && length.doubleValue() <= 90) {
-//           if (weight.doubleValue() >= 1 && weight.doubleValue() <= 10){
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 4.4)));
-//           }else if(weight.doubleValue() >= 10 && weight.doubleValue() <= 20){
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 4)));
-//           }else if(weight.doubleValue() >= 20 && weight.doubleValue() <= 50) {
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 2.4)));
-//           }else{
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 1.5)));
-//           }
-//       } else if(height.doubleValue() > 90
-//               && width.doubleValue() > 90
-//               && length.doubleValue() > 90){
-//           if (weight.doubleValue() >= 1 && weight.doubleValue() <= 10){
-//           createPackageDto
-//                   .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 4)));
-//           }else if(weight.doubleValue() >= 10 && weight.doubleValue() <= 20){
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 3.6)));
-//           }else if(weight.doubleValue() >= 20 && weight.doubleValue() <= 50){
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 2)));
-//           }else{
-//               createPackageDto
-//                       .setTax(BigDecimal.valueOf((calculatedTax) + (calculatedTax / 1.5)));
-//           }
-//       }
-
-
         Package packageToAdd = new Package();
-
         packageToAdd.setContent(content);
         packageToAdd.setDeliveryTax(calculateTax(weight, height, length, width));
 
@@ -112,7 +46,7 @@ public class PackageServiceImpl implements PackageService {
     private BigDecimal calculateTax(BigDecimal weight, BigDecimal height, BigDecimal length, BigDecimal width) {
 
         double calculatedTax = 0.1 * (weight.doubleValue() + length.doubleValue() +
-                        width.doubleValue() + height.doubleValue());
+                width.doubleValue() + height.doubleValue());
 
         double weightMultiplier = calculateWeightMultiplier(weight.doubleValue());
 
@@ -124,23 +58,34 @@ public class PackageServiceImpl implements PackageService {
     }
 
     private double calculateWeightMultiplier(double weight) {
-        if(weight <= 1){
+        if (weight <= 1) {
             return 1.1;
         }
-        if(weight > 1 && weight <= 10){
+        if (weight > 1 && weight <= 10) {
             return 1.15;
         }
-        if (weight > 10 && weight <= 30){
+        if (weight > 10 && weight <= 30) {
             return 1.20;
         }
-        if (weight > 30 && weight <= 60){
+        if (weight > 30 && weight <= 60) {
             return 1.30;
         }
         return 1.7;
     }
 
-    private double calculateSizeMultiplier(double doubleValue, double doubleValue1, double doubleValue2) {
-        //TODO: add calculations
-        return 1.1;
+    private double calculateSizeMultiplier(double height, double width, double length) {
+
+        if (height <= 61 && width <= 44 && length <= 37) {
+            return 1.1;
+        }
+        if (height >= 61 && width >= 44 && length >= 37
+                && height <= 90 && width <= 90 && length <= 90) {
+            return 1.15;
+        }
+        if (height > 90 && width > 90 && length > 90
+                && height <= 160 && width <= 160 && length <= 160) {
+            return 1.4;
+        }
+        return 2.1;
     }
 }
