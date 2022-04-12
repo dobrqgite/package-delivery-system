245653b161d7a3074a3e97da29553300dea49b0f
unknown <yoni.vasileva8b@gmail.com>
2022-04-12 22:27:03 +0300

    testing saving new employee to DB

--- src/main/java/com/example/package_delivery_system/web/controllers/AdminController.java
+++ src/main/java/com/example/package_delivery_system/web/controllers/AdminController.java
@@ -1,17 +1,21 @@
 package com.example.package_delivery_system.web.controllers;
 
-import com.example.package_delivery_system.services.VehicleService;
+import com.example.package_delivery_system.data.dtos.employeeDtos.CreateDriverDto;
+import com.example.package_delivery_system.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.io.IOException;
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final AdminService adminService;
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
 
     @GetMapping("/admin-home")
@@ -19,10 +23,13 @@ public class AdminController {
         return "/admin/admin_home";
     }
 
-    @RequestMapping(value = "/register-driver", method = RequestMethod.POST)
-    public String registerDriver() {
-        return "/admin/regisrer_driver";
+    @RequestMapping(value = "/register-employee", method = RequestMethod.POST)
+    public String registerDriver(CreateDriverDto dto) {
+        this.adminService.registerDriver(dto);
+        return "/admin/admin_home";
     }
+
+
 
 
 }
