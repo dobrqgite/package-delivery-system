39583dc79d217517f542d4bb0bcee5b18b178d2f
George Krumov <61293783+georgik1@users.noreply.github.com>
2022-04-12 13:31:19 +0300

    Delete src/main/java/com/example directory

--- src/main/java/com/example/package_delivery_system/common/AdminConstants.java
+++
@@ -1,5 +0,0 @@
-package com.example.package_delivery_system.common;
-
-/*contains static final messages*/
-public class AdminConstants {
-}
--- src/main/java/com/example/package_delivery_system/data/dtos/packageDtos/PackageResponseDto.java
+++
@@ -1,6 +0,0 @@
-package com.example.package_delivery_system.data.dtos.packageDtos;
-
-public class PackageResponseDto {
-
-    //TODO:add fields to return
-}
