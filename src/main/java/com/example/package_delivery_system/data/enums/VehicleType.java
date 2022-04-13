a8b063f699bee28363db2e58927eda273a3c371b
George Krumov <61293783+georgik1@users.noreply.github.com>
2022-04-13 16:18:48 +0300

    Delete src/main directory

--- src/main/java/com/example/package_delivery_system/data/enums/VehicleType.java
+++
@@ -1,5 +0,0 @@
-package com.example.package_delivery_system.data.enums;
-
-public enum VehicleType {
-    B, C1, C1_PLUS_E, C, C_PLUS_E, A
-}
--- src/main/java/com/example/package_delivery_system/exceptions/VerificationException.java
+++
@@ -1,8 +0,0 @@
-package com.example.package_delivery_system.exceptions;
-
-public class VerificationException extends RuntimeException {
-
-    public VerificationException(String message) {
-        super(message);
-    }
-}
