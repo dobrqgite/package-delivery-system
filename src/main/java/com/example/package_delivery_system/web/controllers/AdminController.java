4e0913d00bb70622681c85b8d7c1aace51814fe6
George Krumov <61293783+georgik1@users.noreply.github.com>
2022-04-14 13:49:32 +0300

    Updated the admin-home view

--- src/main/java/com/example/package_delivery_system/web/controllers/AdminController.java
+++ src/main/java/com/example/package_delivery_system/web/controllers/AdminController.java
@@ -1,26 +1,37 @@
 package com.example.package_delivery_system.web.controllers;
 
 import com.example.package_delivery_system.data.dtos.employeeDtos.EmployeeRegisterDto;
+import com.example.package_delivery_system.data.entities.UserEntity;
+import com.example.package_delivery_system.data.repositories.UserRepository;
 import com.example.package_delivery_system.services.api.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService adminService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public AdminController(AdminService adminService) {
+    public AdminController(AdminService adminService, UserRepository userRepository) {
         this.adminService = adminService;
+        this.userRepository = userRepository;
     }
 
 
     @RequestMapping(value = "/admin-home", method = RequestMethod.GET)
-    public String adminHome() {
+    public String adminHome(Model model) {
+
+        List<UserEntity> allCustomers = this.userRepository.findAll();
+        model.addAttribute("customers", allCustomers);
+
         return "/admin/admin_home";
     }
 
