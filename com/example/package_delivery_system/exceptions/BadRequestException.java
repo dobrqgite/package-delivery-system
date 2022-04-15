3b569b3229648e62b9db36bb24fcd37e7a350117
Dimitar Balimezov <77362152+dbalimezov@users.noreply.github.com>
2022-04-15 13:01:51 +0300

    Delete com/example/package_delivery_system directory

--- com/example/package_delivery_system/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.package_delivery_system.exceptions;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
